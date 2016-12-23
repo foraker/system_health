6f6ab01e2eafda66defb8ca4dbed6101dc518cdd
Stirling Olson <seo@foraker.com>
2016-12-23 13:46:09 -0700

    updating interface

--- lib/system_health/monitor.rb
+++ lib/system_health/monitor.rb
@@ -4,27 +4,16 @@ module SystemHealth
       @monitor_classes = monitor_classes
     end
 
-    def as_json(options = nil)
-      {
-        error_count: error_count,
-        messages: messages
-      }
-    end
-
-    def http_status
-      error_count > 0 ? 500 : 200
-    end
-
-    private
-    attr_reader :monitor_classes
-
     def error_count
       monitors.sum(&:error_count)
     end
 
-    def messages
+    def error_messages
       monitors.flat_map(&:error_messages)
     end
+
+    private
+    attr_reader :monitor_classes
 
     def monitors
       @monitors ||= monitor_classes.map(&:new)
