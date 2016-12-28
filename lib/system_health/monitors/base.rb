ac5bce37766e0407958bcb0e632ca4f31725b8b9
Stirling Olson <seo@foraker.com>
2016-12-28 15:52:11 -0700

    improving interface

--- lib/system_health/monitors/base.rb
+++ lib/system_health/monitors/base.rb
@@ -12,14 +12,22 @@ module SystemHealth
         error_messages.count
       end
 
-      private
-
-      def add_error_messages
+      def description
         raise
       end
 
+      private
+
+      def add_error_messages
+        add_error_message(description) if bad_data?
+      end
+
       def add_error_message(message)
-        error_messaages << message
+        @error_messages << message
+      end
+
+      def bad_data?
+        raise
       end
     end
   end
